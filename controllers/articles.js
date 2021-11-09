c681e0a5cfc4af92253d75942112ca561c2ce800
devinrbopp <devinrbopp@gmail.com>
2021-11-09 13:11:33 -0700

    caught up with class, comments can be added

--- controllers/articles.js
+++ controllers/articles.js
@@ -32,16 +32,32 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then((article) => {
     if (!article) throw Error()
-    console.log(article.author)
+    console.log('this is the article!!!!!!!!!', article)
     res.render('articles/show', { article: article })
   })
   .catch((error) => {
     console.log(error)
     res.status(400).render('main/404')
+  })
+})
+
+// POST --> /articles/:id/comments
+router.post('/:id/comments', (req, res) => {
+  db.comment.create({
+    name: req.body.name,
+    content: req.body.content,
+    articleId: req.params.id
+  })
+  .then(resPost => {
+    console.log('comment?', resPost)
+    res.redirect(`/articles/${req.params.id}`)
+  })
+  .catch(err => {
+    res.status(404).render('main/404')
   })
 })
 
