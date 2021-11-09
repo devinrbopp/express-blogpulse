75f747f69bbd25f2de53ed80ed6e0df34b9714c6
devinrbopp <devinrbopp@gmail.com>
2021-11-09 10:32:41 -0700

    added comments table to db

--- models/article.js
+++ models/article.js
@@ -8,6 +8,8 @@ module.exports = (sequelize, DataTypes) => {
   article.associate = function(models) {
     // associations can be defined here
     models.article.belongsTo(models.author)
+    // one article has many comments
+    models.article.hasMany(models.comment)
   }
   return article
 }
